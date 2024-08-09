8b695fb9ac7b223ad9644b334ed24fcca8e35329
Ubaid2116 <ukubaid74@gmail.com>
2024-08-09 20:25:52 +0500

    Fixed module not found errors and updated dependencies

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,9 +3,11 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-between">
-        <span className="text-xl font-bold tracking-wide ml-4">Ubaid</span>
-        <p className="text-slate-600">Crafting with Passion & Precision.</p>
+      <div className="container p-6 sm:p-12 flex flex-col sm:flex-row justify-between items-center">
+        <span className="text-lg sm:text-xl font-bold tracking-wide">Ubaid</span>
+        <p className="text-slate-600 text-sm sm:text-base text-center sm:text-left mt-2 sm:mt-0">
+          Crafting with Passion & Precision.
+        </p>
       </div>
     </footer>
   );
