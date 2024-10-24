74ea9bcfa8bef8a601e2309e3529c56bbafd1958
Ubaid2116 <ukubaid74@gmail.com>
2024-10-25 00:50:18 +0500

    update code

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 const EmailSection = () => {
   return (
     <section
-      className="grid md:grid-cols-2 my-12 p-32 gap-4 relative"
+      className="grid md:grid-cols-2 my-12 p-32 gap-4 relative bg-black"
       id="contact-section"
     >
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
